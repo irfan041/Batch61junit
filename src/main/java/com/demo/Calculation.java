0caecf76281fb8f6e3929ab57e1469a35541dd1f
irfan041 <er.irfan041@gmail.com>
2022-06-22 12:32:08 -0400

    Project Upload

---
+++ src/main/java/com/demo/Calculation.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Calculation {
+	
+	public int add(int x,int y) {
+		return x+y;
+	}
+	public int subtract(int x,int y) {
+		return x-y;
+	}
+
+}
---
+++ src/test/java/com/demo/CalculatorTest.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	Calculation c=new Calculation();
+	
+	@Test
+	public void test1() {
+		assertEquals(c.add(10, 20), 30);
+		
+	}
+	@Test
+	public void test2() {
+		assertEquals(c.add(10, 20), 10);
+		
+	}
+
+}
